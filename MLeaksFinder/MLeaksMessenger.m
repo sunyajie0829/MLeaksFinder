872e1c9736e769706ac69a3fc1048cfba0c668f8
WSTK-Felix <yajie.sun@weistek.net>
2020-09-08 08:46:11 +0800

    1.废弃 alertView; 2.启用内存溢出查找;

--- MLeaksFinder/MLeaksMessenger.m
+++ MLeaksFinder/MLeaksMessenger.m
@@ -20,20 +20,65 @@ static __weak UIAlertView *alertView;
     [self alertWithTitle:title message:message delegate:nil additionalButtonTitle:nil];
 }
 
+//+ (void)alertWithTitle:(NSString *)title
+//               message:(NSString *)message
+//              delegate:(id<UIAlertViewDelegate>)delegate
+// additionalButtonTitle:(NSString *)additionalButtonTitle {
+//    [alertView dismissWithClickedButtonIndex:0 animated:NO];
+//    UIAlertView *alertViewTemp = [[UIAlertView alloc] initWithTitle:title
+//                                                            message:message
+//                                                           delegate:delegate
+//                                                  cancelButtonTitle:@"OK"
+//                                                  otherButtonTitles:additionalButtonTitle, nil];
+//    [alertViewTemp show];
+//    alertView = alertViewTemp;
+//
+//    NSLog(@"%@: %@", title, message);
+//}
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
               delegate:(id<UIAlertViewDelegate>)delegate
  additionalButtonTitle:(NSString *)additionalButtonTitle {
-    [alertView dismissWithClickedButtonIndex:0 animated:NO];
-    UIAlertView *alertViewTemp = [[UIAlertView alloc] initWithTitle:title
-                                                            message:message
-                                                           delegate:delegate
-                                                  cancelButtonTitle:@"OK"
-                                                  otherButtonTitles:additionalButtonTitle, nil];
-    [alertViewTemp show];
-    alertView = alertViewTemp;
-    
-    NSLog(@"%@: %@", title, message);
+//    [alertView dismissWithClickedButtonIndex:0 animated:NO];
+//    UIAlertView *alertViewTemp = [[UIAlertView alloc] initWithTitle:title
+//                                                            message:message
+//                                                           delegate:delegate
+//                                                  cancelButtonTitle:@"OK"
+//                                                  otherButtonTitles:additionalButtonTitle, nil];
+//    [alertViewTemp show];
+//
+//    alertView = alertViewTemp;
+//
+//    NSLog(@"%@: %@", title, message);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *ac = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *cfa = [UIAlertAction actionWithTitle:additionalButtonTitle style:UIAlertActionStyleDefault handler:nil];
+        [ac addAction:cfa];
+        UIViewController *svc = nil;
+        if (@available(iOS 13.0,*)){
+            UIWindow *keyWindow = nil;
+            for (UIWindowScene *ws in UIApplication.sharedApplication.connectedScenes) {
+                if (!keyWindow)
+                {
+                    if (ws.activationState == UISceneActivationStateForegroundActive || UIApplication.sharedApplication.connectedScenes.count == 1){
+                        for (UIWindow *w in ws.windows) {
+                            if (w.isKeyWindow || UIApplication.sharedApplication.connectedScenes.count == 1){
+                                keyWindow = w;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            svc = keyWindow.rootViewController;
+        }else{
+            svc = UIApplication.sharedApplication.keyWindow.rootViewController;
+        }
+        while (svc.presentedViewController != nil){
+            svc = svc.presentedViewController;
+        }
+        [svc presentViewController:ac animated:true completion:nil];
+        NSLog(@"%@: %@", title, message);
+    });
 }
-
 @end
